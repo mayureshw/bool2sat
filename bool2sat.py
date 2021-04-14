4cea7004d8b25303989b538984fddbe60315013d
mayureshw <mayureshw@github>
2021-04-14 09:20:19 +0530

    Revamped to support construction by equations, CLI removed

--- bool2sat.py
+++ bool2sat.py
@@ -1,5 +1,6 @@
 # We reuse the parser from the dd package, but we do not use bdds
 from dd._parser import Parser
+from functools import reduce
 import subprocess
 import sys
 class CNF:
@@ -29,6 +30,11 @@ class CNF:
         newid = self.nextid()
         CNF._varid[v] = newid
         return newid
+    def newvar(self):
+        newid = self.nextid()
+        newvar = '_v' + str(newid)
+        CNF._varid[newvar] = newid
+        return newid,newvar
     def nextid(self):
         CNF.nodecntr = CNF.nodecntr + 1
         return CNF.nodecntr
@@ -38,12 +44,14 @@ class CNF:
             for sumspec in self.cnftbl[node.operator]] +
         [cnf for c in node.operands if c.type=='operator' for cnf in self._cnf(c)]
         ) if node.type == 'operator' else [[node.id]]
-    def visit(self,node,givenid=None):
+    # opvar ANDing is added on the fly to allow easy merging of formulas
+    def cnf(self): return self.cnfworoot + [[self.opvarid]]
+    def visit(self,node):
         if node.type == 'operator':
-            node.id = givenid if givenid!=None else self.nextid()
+            node.id = self.nextid()
             for arg in node.operands: self.visit(arg)
         else: node.id = self.varid(node.value)
-    def dimacs(self): return ' '.join(' '.join(str(d) for d in c+[0]) for c in self.cnf)
+    def dimacs(self): return ' '.join(' '.join(str(d) for d in c+[0]) for c in self.cnf())
 
     # Generate cnf indimacs form in cnfopfile
     def dump(self):
@@ -52,33 +60,59 @@ class CNF:
     # Run minisat and report results and if satisfiable decode the solution into user's variables
     # eliminating intermediate variables
     def minisat(self):
+        self.dump()
         satop = subprocess.run(['minisat',self.cnfopfile,self.satopfile],
             capture_output=True, encoding='ascii')
         print(satop.stdout)
         if satop.returncode == 10:
             print('Decoding the satisfiable assigment...')
-            idvar = { i:v for v,i in CNF._varid.items() }
+            idvar = { i:v for v,i in self._varid.items() if i in self.inpvars }
             with open(self.satopfile) as fd:
                 satsoln = [int(i) for i in fd.readlines()[-1].split()[:-1]]
             usersoln = [('~' if i<0 else '') + idvar[abs(i)] for i in satsoln if abs(i) in idvar]
             print(' '.join(usersoln))
             
-    def __init__(self,formula,opvar=None):
-        try: root = self.parser.parse(formula)
+    def __and__(self,rval):
+        o = CNF()
+        o.cnfworoot = self.cnfworoot + rval.cnfworoot
+        o.opvarid,_ = self.newvar()
+        o.inpvars = self.inpvars.union(rval.inpvars) - set([self.opvarid,rval.opvarid])
+        return o
+
+    # We follow factory pattern of construction, to make it easy to construct blank objects
+    # which is required in operator overloading for and
+
+    # Construction method by naming an opvar and a formula (string)
+    @classmethod
+    def byformula(cls,opvar,formula):
+        o = CNF()
+        oformula = ''.join([
+            '~(',
+            '(',formula,')', ' ^ ', opvar,
+            ')'
+            ])
+        try: root = o.parser.parse(oformula)
         except Exception:
             print('parse error')
             sys.exit()
-        if opvar and root.type != 'operator':
-            print('opvar argument not supported for atomic formulas')
-            sys.exit()
-        rootid = self.varid(opvar) if opvar else 1
-        self.visit(root,rootid)
-        self.cnf = self._cnf(root) + [[rootid]]
+        o.opvarid = o.varid(opvar)
+        o.visit(root)
+        o.cnfworoot = o._cnf(root)
+        o.inpvars = { v for p in o.cnfworoot for v in p if v != o.opvarid }
+        return o
+
+    # Method to construct cnf from a number of variables in a dictionary eqns and top opvar
+    @classmethod
+    def byequations(cls,opvar,eqns): return reduce(
+        lambda l,r: l&r,
+        [ CNF.byformula(o,f) for o,f in eqns.items() ]
+        )
     
-# If invoked as command, reads boolean formula from filename if specified as
-# argument else from stdin. Produces cnf.txt and satop.txt and minisat stdout
+# When opvar support was added CLI was removed. A new CLI may be added later
+# This is merely a test driver now
 if __name__=='__main__':
-    fd = sys.stdin if len(sys.argv)<2 else open(sys.argv[1])
-    cnf = CNF(fd.read())
-    cnf.dump()
-    cnf.minisat()
+    cnf1 = CNF.byformula('p','a&b')
+    cnf2 = CNF.byformula('q','p&d')
+    cnf3 = cnf1 & cnf2
+    print(cnf3.cnf())
+    cnf3.minisat()
